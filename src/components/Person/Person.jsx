3aa12d25f0aad6cf91112a3dc14ea9d12671dc51
Petro <fox4income@gmail.com>
2025-07-18 19:27:48 +0300

    rendered

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,16 +4,14 @@ export const Person = ({ person }) => (
     <p className="Person__age">
       {person.age !== undefined ? `I am ${person.age}` : ''}
     </p>
-    <p className="Person__isMarried">
-      {person.isMarried === false ? (
-        'I am not married'
-      ) : (
-        <p className="Person__partner">
-          {person.partnerName !== undefined
-            ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-            : ''}
-        </p>
-      )}
-    </p>
+    {person.isMarried === false ? (
+      <p className="Person__isMarried">I am not married</p>
+    ) : (
+      <p className="Person__partner">
+        {person.partnerName !== undefined
+          ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+          : ''}
+      </p>
+    )}
   </section>
 );
